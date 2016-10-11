3389e437ea0f6220c1d02248ef1664a578ea018d
Vladimir Mikhno <vladimir.mikhno@dunicealliance.com>
2016-10-11 11:51:41 +0300

    update universal funcs

--- universal/index.js
+++ universal/index.js
@@ -2,16 +2,13 @@
 
 var express = require('express');
 var controller = require('./universal.controller.js');
-import * as auth from '../../auth/auth.service'
-import {checkEnabledTables} from './universal.middleware.js'
 
 var router = express.Router();
 
-router.get('/:model',auth.isAuthenticated(),checkEnabledTables('GET'), controller.index);
-// router.get('/:id', controller.show);
-router.post('/:model', auth.isAuthenticated(),checkEnabledTables('POST'), controller.create);
-router.put('/:model', auth.isAuthenticated(),checkEnabledTables('PATCH'), controller.upsert);
-router.patch('/:id', controller.patch);
-// router.delete('/:id', controller.destroy);
+router.get('/:model', controller.index);
+router.post('/:model', controller.create);
+router.put('/:model', controller.upsert);
+router.patch('/:model', controller.patch);
+router.delete('/:model', controller.destroy);
 
 module.exports = router;
--- universal/universal.controller.js
+++ universal/universal.controller.js
@@ -19,40 +19,17 @@ function respondWithResult(res, statusCode,limit, offset, haveNestedWhere,count)
   statusCode = statusCode || 200;
   return function(entity) {
     if(entity) {
+      var resObj ={};
+      if(count) resObj.count=entity.length;
       // We created limit and offset at server side( not at DB side) until this bug will be fixed https://github.com/sequelize/sequelize/issues/3007
-      if((limit||offset) && haveNestedWhere) {
+      if((limit||offset)){
         var limitedEntity= _.cloneDeep(entity).slice(offset || 0,+(offset || 0) + +limit)
       }
-      var resObj = {data:limitedEntity|| entity}
-      if(count) resObj.count=resObj.data.length;
+      resObj.data =limitedEntity|| entity;
 
       return res.status(statusCode).json(resObj);
     }
     return res.status(statusCode).json({status:'updated'});;
-  };
-}
-
-function patchUpdates(patches) {
-  return function(entity) {
-    try {
-      jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
-      return Promise.reject(err);
-    }
-
-    return entity.save();
-  };
-}
-
-
-function removeEntity(res) {
-  return function(entity) {
-    if(entity) {
-      return entity.destroy()
-        .then(() => {
-          res.status(204).end();
-        });
-    }
   };
 }
 
@@ -69,6 +46,7 @@ function handleEntityNotFound(res) {
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
   return function(err) {
+    console.log(err,'err')
     res.status(statusCode).send(err);
   };
 }
@@ -94,13 +72,13 @@ export function index(req, res) {
     var agregateString = req.query.agregate.slice(1,req.query.agregate.length -1).split(',');
     generateAgregateList(agregateString,selector,req.params.model);
   }
-  if(req.query.limit){
-    // We will create limit and offset at server side( not at DB side) until this bug will be fixed https://github.com/sequelize/sequelize/issues/3007
-    if(!haveNestedWhere) selector.limit = +req.query.limit;
-  }
-  if(req.query.offset){
-    if(!haveNestedWhere)  selector.offset = +req.query.offset;
-  }
+  // if(req.query.limit){
+  //   // We created limit and offset at server side( not at DB side) until this bug will be fixed https://github.com/sequelize/sequelize/issues/3007
+  //   // if(!haveNestedWhere) selector.limit = +req.query.limit;
+  // }
+  // if(req.query.offset){
+  //   // if(!haveNestedWhere)  selector.offset = +req.query.offset;
+  // }
   if(req.query.raw){
     selector.raw = true;
   }
@@ -112,24 +90,13 @@ export function index(req, res) {
   selector.logging = true;
 
   return db[capitalizeFirstLetter(snakeToCamel(req.params.model))].findAll(selector)
-    .then(respondWithResult(res,undefined,req.query.limit,req.query.offset,haveNestedWhere,req.query.count))
-    .catch(handleError(res))
-}
-
-// Gets a single <model> from the DB
-export function show(req, res) {
-  return CompassCopy.find({
-    where: {
-      compass_copy_id: req.params.id
-    }
-  })
-    .then(handleEntityNotFound(res))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+      .then(respondWithResult(res,undefined,req.query.limit,req.query.offset,haveNestedWhere,req.query.count))
+      .catch(handleError(res))
 }
 
 // Creates a new <model> in the DB
 export function create(req, res) {
+  console.log('\n\n\n\n\n\n\n',req.body,'\n\n\n\n\n\n\n')
 
   if(req.body.array){
     var mode ='bulkCreate'
@@ -137,9 +104,9 @@ export function create(req, res) {
 
     req.body.array.forEach((item)=>{
       var oneData = {};
-      Object.keys(item).forEach((itemColumn)=>{oneData[toUnderscore(itemColumn)]= item[itemColumn]});
-      data.push(oneData)
-    })
+    Object.keys(item).forEach((itemColumn)=>{oneData[toUnderscore(itemColumn)]= item[itemColumn]});
+    data.push(oneData)
+  })
   }else{
     var mode ='create'
     var data = {};
@@ -147,8 +114,8 @@ export function create(req, res) {
   }
 
   return db[capitalizeFirstLetter(snakeToCamel(req.params.model))][mode](data)
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
+      .then(respondWithResult(res, 201))
+      .catch(handleError(res));
 }
 
 // Upserts the given <model> in the DB at the specified ID
@@ -160,20 +127,20 @@ export function upsert(req, res) {
 
     req.body.array.forEach((item)=>{
       var oneData = {};
-      Object.keys(item).forEach((itemColumn)=>{oneData[toUnderscore(itemColumn)]= item[itemColumn]});
-      data.push(oneData)
-    });
+    Object.keys(item).forEach((itemColumn)=>{oneData[toUnderscore(itemColumn)]= item[itemColumn]});
+    data.push(oneData)
+  });
 
     return db[capitalizeFirstLetter(snakeToCamel(req.params.model))].bulkCreate(data, {logging:true,updateOnDuplicate: true })
-      .then(respondWithResult(res, 201))
-      .catch(handleError(res));
+        .then(respondWithResult(res, 201))
+        .catch(handleError(res));
 
   }else{
     //' single upsert'
 
     return db[capitalizeFirstLetter(snakeToCamel(req.params.model))].upsert(req.body)
-      .then(respondWithResult(res))
-      .catch(handleError(res));
+        .then(respondWithResult(res))
+        .catch(handleError(res));
   }
 }
 
@@ -193,9 +160,9 @@ export function patch(req, res) {
   if((!req.query.empty) &&(!Object.keys(selector.where).length)) return res.status(500).send({message:'where selector can not be empty without "empty" parameter'});
 
   return db[capitalizeFirstLetter(snakeToCamel(req.params.model))]
-    .update(changes,selector)
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+      .update(changes,selector)
+      .then(respondWithResult(res))
+      .catch(handleError(res));
 }
 
 // Deletes a <model> from the DB
@@ -212,9 +179,9 @@ export function destroy(req, res) {
   if((!req.query.empty) &&(!Object.keys(selector.where).length)) return res.status(500).send({message:'where selector can not be without "empty" parameter'});
 
   return db[capitalizeFirstLetter(snakeToCamel(req.params.model))]
-    .destroy(selector)
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+      .destroy(selector)
+      .then(respondWithResult(res))
+      .catch(handleError(res));
 }
 
 //===================================================================================================================
@@ -231,9 +198,10 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function parseValue(array){
-  if(array.length==1) return array[0].toLowerCase()=='null'?null:array[0];
-  return array;
+function parseValue(array,operator){
+  if(operator =='$in'|| operator =='$notIn') {return array}
+  else if(array.length==1){return array[0].toLowerCase()=='null'?null:array[0];}
+  else {return array;}
 }
 
 //===================================================================================================================
@@ -241,24 +209,23 @@ function parseValue(array){
 function generateIncludeList(includeList,inputObject){
   var objectWithInclude = inputObject;
 
-   includeList.forEach((item,i)=>{
-     var linkToParent = objectWithInclude;
-     var tables = item.split('.');
+  includeList.forEach((item,i)=>{
+    var linkToParent = objectWithInclude;
+  var tables = item.split('.');
 
-     tables.forEach((tableName,i)=>{
-       var tableName = capitalizeFirstLetter(snakeToCamel(tableName))
-       var linkToModel = !!~tableName.indexOf(':')? db[tableName.slice(0,tableName.indexOf(':'))]:db[tableName];
+  tables.forEach((tableName,i)=>{
+    var tableName = capitalizeFirstLetter(snakeToCamel(tableName))
+    var linkToModel = !!~tableName.indexOf(':')? db[tableName.slice(0,tableName.indexOf(':'))]:db[tableName];
 
 
 
-       if(!linkToParent.include) linkToParent.include=[];
-       if( !_.find(linkToParent.include,({model:linkToModel,as:capitalizeFirstLetter(snakeToCamel(tableName))}))) linkToParent.include.push({model:linkToModel,as:capitalizeFirstLetter(snakeToCamel(tableName))});
-       linkToParent = _.find(linkToParent.include,({model:linkToModel}))
-     })
+  if(!linkToParent.include) linkToParent.include=[];
+  if( !_.find(linkToParent.include,({model:linkToModel,as:capitalizeFirstLetter(snakeToCamel(tableName))}))) linkToParent.include.push({model:linkToModel,as:capitalizeFirstLetter(snakeToCamel(tableName))});
+  linkToParent = _.find(linkToParent.include,({model:linkToModel}))
+})
 
-   });
+});
 }
-
 
 function generateWhereList(whereString,inputObject){
   var conditions = [];
@@ -292,25 +259,25 @@ function generateWhereList(whereString,inputObject){
   conditions.forEach((item,i)=>{
     if(item.path.length) paths.push(item.path.join('.'));
 
-    let newSelector = {};
-    if(!item.path.length){
-      newSelector[item.column] = {};
-      newSelector[item.column][item.operator]=parseValue(item.value);
-    }else{
-      let camelPaths = item.path.map(function(item){return capitalizeFirstLetter(snakeToCamel(item))}).join('.')
-      newSelector[`$${camelPaths}.${item.column}$`]={};
-      newSelector[`$${camelPaths}.${item.column}$`][item.operator]=parseValue(item.value);
-    }
+  let newSelector = {};
+  if(!item.path.length){
+    newSelector[item.column] = {};
+    newSelector[item.column][item.operator]=parseValue(item.value,item.operator);
+  }else{
+    let camelPaths = item.path.map(function(item){return capitalizeFirstLetter(snakeToCamel(item))}).join('.')
+    newSelector[`$${camelPaths}.${item.column}$`]={};
+    newSelector[`$${camelPaths}.${item.column}$`][item.operator]=parseValue(item.value,item.operator);
+  }
 
-    if(item.sign=='AND'){
-      linkToPreviousAnd.$and.push(newSelector)
-    }else if(item.sign=='OR'){
-      linkToPreviousAnd = {$and:[]}
-      linkToPreviousAnd.$and.push(newSelector)
-      inputObject.where.$or.push(linkToPreviousAnd)
-    }
+  if(item.sign=='AND'){
+    linkToPreviousAnd.$and.push(newSelector)
+  }else if(item.sign=='OR'){
+    linkToPreviousAnd = {$and:[]}
+    linkToPreviousAnd.$and.push(newSelector)
+    inputObject.where.$or.push(linkToPreviousAnd)
+  }
 
-  });
+});
 
   generateIncludeList(paths,inputObject)
 
@@ -345,33 +312,34 @@ function generateAttributesList(attributeString,inputObject){
   });
 }
 
-
 function generateAgregateList(attributeList,selector,modelName){
   attributeList[0] = attributeList[0].split('.');
-  attributeList[3] = attributeList[3].split('.');
+  attributeList[3] = (attributeList[3]||'').split('.');
 
   var column = attributeList[0].pop(),
-    path= attributeList[0].map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))}).join('.'),
-    func   = attributeList[1],
-    alias= attributeList[2],
-    groupColumn = attributeList[3].pop(),
-    groupPath  =  attributeList[3].map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))}).join('.'),
-    attributeFlag = attributeList[4];
+      path= attributeList[0].map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))}).join('.'),
+      func   = attributeList[1],
+      alias= attributeList[2],
+      groupColumn = attributeList[3].pop(),
+      groupPath  =  attributeList[3].map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))}).join('.'),
+      attributeFlag = attributeList[4];
 
 
-    selector.attributes = {include:[],exclude:[]}
+  selector.attributes = {include:[],exclude:[]}
+  if(func=='DISTINCT') selector.attributes.exclude = selector.attributes.exclude.concat(Object.keys(db[capitalizeFirstLetter(snakeToCamel(modelName))].tableAttributes))
 
-    if (!path.trim()){
+  if (!path.trim()){
 
-      selector.attributes.include.push([db.sequelize.fn(func, db.sequelize.col(modelName+'.'+column)), alias])
-      if (groupColumn && !groupPath) selector.group= [modelName+'.'+groupColumn]
+    selector.attributes.include.push([db.sequelize.fn(func, db.sequelize.col(modelName+'.'+column)), alias])
+    if (groupColumn && !groupPath) selector.group= [modelName+'.'+groupColumn]
 
-    }else{
-      selector.attributes.include.push([db.sequelize.fn(func, db.sequelize.col(path+'.'+column)), alias])
-      if(groupColumn) selector.group= [groupPath?groupPath:modelName+'.'+groupColumn]
-    }
-
+  }else{
+    selector.attributes.include.push([db.sequelize.fn(func, db.sequelize.col(path+'.'+column)), alias])
+    if(groupColumn) selector.group= [groupPath?groupPath:modelName+'.'+groupColumn]
   }
+
+
+}
 
 function generateOrderList(orderString,selector){
 
@@ -382,22 +350,20 @@ function generateOrderList(orderString,selector){
   orderList.forEach((item,i)=>{
 
     var arr = item.split(',');
-    var ascDesc = arr[1];
-    var arr2 = arr[0].split('.');
-    var columnName = arr2.pop();
-    var tables = arr2.map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))})
+  var ascDesc = arr[1];
+  var arr2 = arr[0].split('.');
+  var columnName = arr2.pop();
+  var tables = arr2.map((item)=>{return capitalizeFirstLetter(snakeToCamel(item))})
 
-    var result = [];
-    tables.forEach((item)=>{
-      var linkToModel = !!~item.indexOf(':')? db[item.slice(0,item.indexOf(':'))]:db[item];
-      result.push({model:linkToModel,as:item})
-    })
-    result.push(columnName);
-    result.push(ascDesc);
+  var result = [];
+  tables.forEach((item)=>{
+    var linkToModel = !!~item.indexOf(':')? db[item.slice(0,item.indexOf(':'))]:db[item];
+  result.push({model:linkToModel,as:item})
+})
+  result.push(columnName);
+  result.push(ascDesc);
 
-
-    selector.order.push(result);
-  })
-
+  selector.order.push(result);
+})
 
 }
